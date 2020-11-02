efb43c88cbaff376bb28fbfc8e6269c3f02dc3ff
TusharPatil010010 <tusharp010010@gmail.com>
2020-11-02 07:47:20 +0530

    Added : added functionality to retrieve the employee payroll from the database

---
+++ src/main/java/com/payrollservice/EmployeePayrollService.java
@@ -0,0 +1,85 @@
+package com.payrollservice;
+
+import java.sql.SQLException;
+import java.util.*;
+
+public class EmployeePayrollService {
+	static Scanner consoleInput = new Scanner(System.in);
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	};
+
+	private List<Employee> employeeList;
+
+	public EmployeePayrollService(List<Employee> list) {
+		this.employeeList = list;
+	}
+
+	public EmployeePayrollService() {
+	}
+
+	public static void main(String[] args) {
+		ArrayList<Employee> list = new ArrayList<Employee>();
+		EmployeePayrollService eService = new EmployeePayrollService(list);
+		System.out.println("Do you want to add data from console");
+		String option = consoleInput.nextLine();
+		do {
+			if (option.equalsIgnoreCase("yes")) {
+				eService.readEmployeePayrollData(IOService.CONSOLE_IO);
+				consoleInput.nextLine();
+				System.out.println("Want to enter again");
+				option = consoleInput.nextLine();
+			}
+		} while (option.equalsIgnoreCase("yes"));
+		eService.writeData(IOService.FILE_IO);
+		eService.readEmployeePayrollData(IOService.FILE_IO);
+	}
+
+	public void writeData(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			System.out.println("Writting data of employee to console: " + employeeList);
+		else if (ioService.equals(IOService.FILE_IO)) {
+			new PayrollFileServiceIO().writeData(employeeList);
+		}
+	}
+
+	public void readEmployeePayrollData(IOService ioService) {
+		List<Employee> list = new ArrayList<>();
+		if (ioService.equals(IOService.CONSOLE_IO)) {
+			System.out.println("Enter the employee id");
+			int id = consoleInput.nextInt();
+			consoleInput.nextLine();
+			System.out.println("Enter the employee name");
+			String name = consoleInput.nextLine();
+			System.out.println("Enter the employee salary");
+			double salary = consoleInput.nextDouble();
+			employeeList.add(new Employee(id, name, salary));
+		} else if (ioService.equals(IOService.FILE_IO)) {
+			list = new PayrollFileServiceIO().readData();
+			System.out.println("Writing data from file" + list);
+		}
+	}
+
+	public void printData(IOService ioService) {
+		if (ioService.equals(IOService.FILE_IO)) {
+			new PayrollFileServiceIO().printData();
+		}
+	}
+
+	public long countEntries(IOService ioService) {
+		long entries = 0;
+		if (ioService.equals(IOService.FILE_IO)) {
+			entries = new PayrollFileServiceIO().countEntries();
+		}
+		System.out.println("No of Entries in File: " + entries);
+		return entries;
+	}
+
+	public List<Employee> readEmployeePayrollDBData(IOService ioService) throws SQLException {
+		if (ioService.equals(IOService.DB_IO)) {
+			this.employeeList = new PayrollFileServiceIO().readData();
+		}
+		return this.employeeList;
+	}
+}
--- src/main/java/com/payrollservice/PayrollServiceJDBC.java
+++ src/main/java/com/payrollservice/PayrollServiceJDBC.java
@@ -3,14 +3,20 @@ package com.payrollservice;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class PayrollServiceJDBC {
-	public static void main(String[] args) {
+	private Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String userName = "root";
 		String password = "open";
-		Connection connection;
+		Connection connection = null;
 		// loading the driver
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -27,6 +33,7 @@ public class PayrollServiceJDBC {
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+		return connection;
 	}
 
 	private static void listDrivers() {
@@ -35,5 +42,26 @@ public class PayrollServiceJDBC {
 			Driver driverClass = (Driver) driverList.nextElement();
 			System.out.println("  " + driverClass.getClass().getName());
 		}
+	}
+
+	public List<Employee> readData() throws SQLException {
+		String sql = "Select * from payroll_service; ";
+		List<Employee> employeeData = new ArrayList<>();
+		try (Connection connection = this.getConnection();) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			while (result.next()) {
+				int id = result.getInt("id");
+				String name = result.getString("name");
+				double salary = result.getDouble("salary");
+				LocalDate start = result.getDate("start").toLocalDate();
+				employeeData.add(new Employee(id, name, salary, start));
+			}
+		} catch (SQLException exception) {
+			System.out.println(exception);
+		} catch (Exception exception) {
+			throw new SQLException("Unable to execute query");
+		}
+		return employeeData;
 	}
 }
