b92d369ceb96d074c33285f3c80af95b7343d320
TusharPatil010010 <tusharp010010@gmail.com>
2020-11-02 07:27:51 +0530

    Added : added functionality to connect java program to database

--- src/main/java/com/payrollservice/PayrollServiceJDBC.java
+++ src/main/java/com/payrollservice/PayrollServiceJDBC.java
@@ -1,5 +1,39 @@
 package com.payrollservice;
 
-public class PayrollServiceJDBC {
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 
+public class PayrollServiceJDBC {
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "open";
+		Connection connection;
+		// loading the driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loaded Successfully.");
+		} catch (ClassNotFoundException exception) {
+			throw new IllegalStateException("Cannot find driver in the classpath...", exception);
+		}
+		listDrivers();
+		// creating a connection
+		try {
+			System.out.println("Connecting to database..... " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successful: " + connection);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("  " + driverClass.getClass().getName());
+		}
+	}
 }
